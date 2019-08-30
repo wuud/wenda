dd389115423c8d99855bfc48340469a2a325421b
wuud <wuu_dd@qq.com>
2019-08-30 20:59:29 +0800

    s

--- wenda 1.0/src/main/java/cn/wenda/controller/SearchController.java
+++ wenda 1.0/src/main/java/cn/wenda/controller/SearchController.java
@@ -1,6 +1,9 @@
 package cn.wenda.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,8 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import cn.wenda.model.EntityType;
 import cn.wenda.model.Question;
+import cn.wenda.service.FollowService;
 import cn.wenda.service.QuestionService;
+import cn.wenda.service.SearchService;
+import cn.wenda.service.UserService;
 
 @Controller
 public class SearchController {
@@ -18,12 +25,36 @@ public class SearchController {
 	@Autowired
 	QuestionService questionService;
 	
-	@RequestMapping(value="/search",method=RequestMethod.POST)
-	public String search(@RequestParam("word")String word,Model model) {
-		System.out.println(word);
-		List<Question> questions = questionService.containsWord(word);
-		model.addAttribute("questions",questions);
-		return "explore";
+	@Autowired
+	SearchService searchService;
+	@Autowired
+	FollowService followService;
+	@Autowired
+	UserService userService;
+	
+	@RequestMapping(value="/search",method=RequestMethod.GET)
+	public String search(Model model, @RequestParam("q") String keyword,
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "count", defaultValue = "10") int count) {
+		try {
+			List<Question> questionList = searchService.searchQuestion(keyword, offset, count,"<em>", "</em>");
+			List<Question> questions=new ArrayList<>();
+            for (Question question : questionList) {
+                Question q = questionService.getQuestionById(question.getId());
+                if (question.getContent() != null) {
+                    q.setContent(question.getContent());
+                }
+                if (question.getTitle() != null) {
+                    q.setTitle(question.getTitle());
+                }
+                questions.add(q);
+            }
+            model.addAttribute("questions",questions);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "result";
 	}
 
 }
--- wenda 1.0/src/test/java/cn/wenda/test/JedisTest.java
+++ wenda 1.0/src/test/java/cn/wenda/test/JedisTest.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+import org.springframework.transaction.annotation.Transactional;
 
 public class JedisTest {
-	
+
 	@Test
 	public void test() {
 //		Map<Integer,String> map=new HashMap<>();
@@ -14,14 +15,17 @@ public class JedisTest {
 //		map.put(2, "sdf");
 //		map.put(3, "HUi");
 //		System.out.println(map);
-		List<String> list=new ArrayList<>();
+		List<String> list = new ArrayList<>();
 		list.add("1");
 		list.add("2");
 		list.add("3");
-		for(String s:list) {
+		for (String s : list) {
 			System.out.println(s);
 		}
-	
+
+	}
+	@Transactional
+	public void tx() {
 	}
 
 }
